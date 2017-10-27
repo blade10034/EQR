d86568540710343333a5db10be0f96839f038264
Samuel Park <spark@eqri.com>
2017-10-27 11:18:50 -0400

    added timesheet header

--- app/components/timesheet/Timesheet.jsx
+++ app/components/timesheet/Timesheet.jsx
@@ -3,11 +3,26 @@ import { Switch, Route } from 'react-router-dom';
 import FullList from './FullList.jsx';
 import Week from './Week.jsx';
 
+class TimesheetHeader extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Timesheet</h1>
+                <ul>
+                    <li>Employee First Name</li>
+                    <li>Employee Last Name</li>
+                    <li>Employee Number</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
 export default class Timesheet extends React.Component {
     render() {
         return (
             <div>
-                <h1>Timesheet</h1>
+                <TimesheetHeader />
                 <Switch>
                     <Route exact path='/timesheet' component={FullList}/>
                     <Route path='/timesheet/:number' component={Week}/>
